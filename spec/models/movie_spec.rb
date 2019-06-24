af973e82f7286b93fb6c481cea9da49aa4ace6d2
David <sanchez.dav90@gmail.com>
2019-06-23 21:35:51 -0500

    Remove description from movie in spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 # Test suite for the Movie model
 RSpec.describe Movie, type: :model do
   it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
 end

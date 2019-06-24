c54663ab0738a52c07c810fd368e6f7692c52321
David <sanchez.dav90@gmail.com>
2019-06-23 21:13:58 -0500

    Create genre model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_012131) do
+ActiveRecord::Schema.define(version: 2019_06_24_020615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,15 +46,21 @@ ActiveRecord::Schema.define(version: 2019_06_17_012131) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title", default: "", null: false
-    t.text "description", default: "", null: false
     t.integer "year"
     t.string "duration"
     t.string "genre"
     t.string "maturity_rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
